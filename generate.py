50f768b47d5a74b024f9f44eedb741bf9118a990
richardhp <richardhp@gmail.com>
2019-07-04 14:39:18 +1200

    Main file for the project

---
+++ generate.py
@@ -0,0 +1,4 @@
+possible_titles = ["Welcome", "Getting Started", "How to Contribute", "Feedback", "Contributing", "Installation", "Building the Project"]
+
+possible_languages = ["Python", "Java", "NodeJs", "Haskell", "Rust", "C", "C++", "Scala", "Ruby", "Fortran", "Lua"]
+
